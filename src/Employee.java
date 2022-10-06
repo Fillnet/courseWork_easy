fb0280902f1ae4ea217d2066677a73ca2545a457
fillnet <filipp@tenikaytis.ru>
2022-10-06 13:31:20 +0300

    HasBegun

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    private final String firstName;
+    private final String lastName;
+    private final String patronymic;
+
+    public Employee(String firstName, String lastName, String patronymic) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Employee ivanov = new Employee("Ivan", "Ivanov", "Ivanovich");
+        Employee petrov = new Employee("Petryxa", "Petrov", "Valerievich");
+        Employee dylan = new Employee("Bob", "Dylan", "Grammy");
+        Employee cobain = new Employee("Kurt", "Cobain", "Donald");
+        Employee meladze = new Employee("Valerii", "Meladze", "Marmeladze");
     }
 }
