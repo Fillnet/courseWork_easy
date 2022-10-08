76bf0c4cd8e7e4064d273ce393149bf7a6065ce1
Fillnet <filipp@tenikaytis.ru>
2022-10-09 00:26:46 +0300

    HasBegun

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.Random;
 
 public class Employee {
@@ -6,20 +8,42 @@ public class Employee {
     private int salary;
     private final int id;
     private static int count = 0;
+    static int[] sal = generateRandomArray(10);
+    public static int[] generateRandomArray(int size) {
+        int[] array = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(( 100000)) + 100000;
+        }
+        return array;
+    }
+    public static int calculateAverage(int[] array) {
+        int sum = sumArray(array);
+        return sum / array.length;
+    }
+
+    public static int sumArray(int[] array) {
+            return Arrays.stream(array).sum();
+        }
+
+    public static OptionalInt getMaxSalary(int[] array) {
+         return Arrays.stream(sal).max();
+    }
 
 
-    public Employee(String fullname, int salary) {
+    public Employee(String fullname) {
         this.fullname = fullname;
         this.salary = salary;
         this.id = ++count;
+        this.sal[id] = sal[count];
     }
 
-    public int getSalary() {
-        return salary;
+    public static int getSalary() {
+        return sal[count];
     }
 
-    public int setSalary(int salary) {
-        return salary;
+    public int setSalary(int sal[]) {
+        return sal[count];
     }
 
     public int getId() {
@@ -28,7 +52,7 @@ public class Employee {
 
     @Override
     public String toString() {
-        return id + "." + fullname + " = " + salary;
+        return id + "." + fullname + " =  "  + sal[id];
     }
 
     public String employess() {
@@ -37,3 +61,4 @@ public class Employee {
 
 
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,35 @@ public class Main {
     private static final Employee[] employess = new Employee[10];
 
     public static void main(String[] args) {
-        employess[0] = new Employee("Ivan Ivanov Ivanovich", 100000);
-        employess[1] = new Employee("Petryxa Petrov Valerievich", 150000);
-        employess[2] = new Employee("Bob Dylan Grammy", 160000);
-        employess[3] = new Employee("Kurt Cobain Donald", 125000);
-        employess[4] = new Employee("Valerii Meladze Marmeladze", 115000);
+        employess[0] = new Employee("Ivan Ivanov Ivanovich");
+        employess[1] = new Employee("Petryxa Petrov Valerievich");
+        employess[2] = new Employee("Bob Dylan Grammy");
+        employess[3] = new Employee("Kurt Cobain Donald");
+        employess[4] = new Employee("Valerii Meladze Marmeladze");
         System.out.println(employess[0]);
         System.out.println(employess[1]);
         System.out.println(employess[2]);
         System.out.println(employess[3]);
         System.out.println(employess[4]);
-        System.out.println(" Сумма зарплат всех сотрудников равна: " + summarySalary());
+        int[] sal = Employee.generateRandomArray(10);
+        System.out.println(" Сумма зарплат всех сотрудников равна: " + Employee.sumArray(sal));
+        System.out.println(" Средняя зарплата сотрудников равна: " + Employee.calculateAverage(sal));
         System.out.println(" Максимальная зарплата у сотрудника: " + getMaxSalary());
         System.out.println(" Минимальная зарплата у сотрудника: " + getMinSalary());
-//        System.out.println(getAverageValueSalary());
-    }
 
-    public static int summarySalary() {
-        int sum = 0;
+    }
+    public static Employee getMinSalary() {
+        int min = Integer.MAX_VALUE;
+        Employee workMinEmployee = null;
         for (Employee employee : employess) {
-            if (employee != null)
-                sum += employee.getSalary();
+            if (employee != null && employee.getSalary() < min) {
+                min = employee.getSalary();
+                workMinEmployee = employee;
+            }
         }
-        return sum;
+        return workMinEmployee;
     }
-
-    public static Employee getMaxSalary() {
+        public static Employee getMaxSalary() {
         int max = Integer.MIN_VALUE;
         Employee workMaxEmployee = null;
         for (Employee employee : employess) {
@@ -39,21 +42,13 @@ public class Main {
         return workMaxEmployee;
     }
 
-    public static Employee getMinSalary() {
-        int min = Integer.MAX_VALUE;
-        Employee workMinEmployee = null;
-        for (Employee employee : employess) {
-            if (employee != null && employee.getSalary() < min) {
-                min = employee.getSalary();
-                workMinEmployee = employee;
-            }
-        }
-            return workMinEmployee;
-    }
 
-    public static int getAverageValueSalary() {
-        int averageValueSalary = getAverageValueSalary();
-        return averageValueSalary / employess.length;
-    }
+
+
+
+
+
+
 
 }
+
