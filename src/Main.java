580225b7a746e6b2dc180f5d5875013cf0efa5b1
Fillnet <filipp@tenikaytis.ru>
2022-10-09 18:25:15 +0300

    HasBegun

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@
 public class Main {
     private static final Employee[] employess = new Employee[10];
     public static void main(String[] args) {
-        employess[0] = new Employee("Бухгалтерия","Ivan Ivanov Ivanovich", 125547);
-        employess[1] = new Employee("ИТ","Petryxa Petrov Valerievich", 115326);
-        employess[2] = new Employee("Разработка","Bob Dylan Grammy", 178147);
-        employess[3] = new Employee("Тестировщики", "Kurt Cobain Donald", 136222);
-        employess[4] = new Employee("Разработка","Valerii Meladze Marmeladze", 196158);
+        employess[0] = new Employee("Бухгалтерия","Иванов Иван Иванович", 125547);
+        employess[1] = new Employee("ИТ","Петров Петруха Валерьевич", 115326);
+        employess[2] = new Employee("Разработка","Боб Дилан", 178147);
+        employess[3] = new Employee("Тестировщики", "Курт Дональд Кобейн", 136222);
+        employess[4] = new Employee("Разработка","Меладзе Валерий", 196158);
+        employess[5] = new Employee("Разработка", "Тестовый", 199999);
         fullEmployee();
         System.out.println();
         System.out.println(" Сумма зарплат всех сотрудников равна: " + calculatSumSalary());
@@ -16,33 +17,27 @@ public class Main {
         System.out.println();
         System.out.println("Список сотрудников: ");
         fullName();
-
         }
 //        employess[0].setSalary(555555);
 //        employess[0].setDepartment("Тестировщик");
 //        System.out.println(" Сумма зарплат всех сотрудников равна: " + calculatSumSalary());
 //        System.out.println(" Максимальная зарплата у сотрудника: " + getMaxSalary());
 //        System.out.println(employess[0]);
-
-
     private static void fullName() {
-            System.out.println(employess[0].getFullname());
-            System.out.println(employess[1].getFullname());
-            System.out.println(employess[2].getFullname());
-            System.out.println(employess[3].getFullname());
-            System.out.println(employess[4].getFullname());
-            System.out.println();
+            for(Employee var : employess){
+            if (var != null) {
+            System.out.println(var.getFullname());
+            }
+        }
     }
-
     private static void fullEmployee() {
-        System.out.println(employess[0]);
-        System.out.println(employess[1]);
-        System.out.println(employess[2]);
-        System.out.println(employess[3]);
-        System.out.println(employess[4]);
+        for(Employee var : employess){
+            if (var != null) {
+                System.out.println(var);
+            }
+        }
 
     }
-
     private static int calculatSumSalary() {
         int sum = 0;
         for (Employee employee : employess) {
@@ -67,7 +62,6 @@ public class Main {
             }
             return average/total;
         }
-
     private static Employee getMinSalary() {
         int min = Integer.MAX_VALUE;
         Employee workMinEmployee = null;
@@ -90,7 +84,6 @@ public class Main {
         }
         return workMaxEmployee;
     }
-
 }
 
 
